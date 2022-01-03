a46906d6c96ccd8f42425c4fbc66fadfbb03e4cc
alla <a.barabasch@gmail.com>
2022-01-03 20:48:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
+import Login from "./components/login";
+import React from "react";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
